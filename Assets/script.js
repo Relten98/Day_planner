b79cab2fe1f7c342e21d0f804bfb804cfdf76410
Relten98 <73558735+Relten98@users.noreply.github.com>
2020-12-12 13:48:26 -0800

    f

--- Assets/script.js
+++ Assets/script.js
@@ -1,33 +1,6 @@
-let i = new Date();
+/// Gets current date
+const currentDate = moment().format('MMMM Do YYYY, h:mm:ss a');
 
-$(document).ready(function() {
-    var container = $('.container');
-
-    function timeformat(i) {
-        if (i === 12) {
-            return '12PM';
-        }
-        if (i > 12) {
-            return `${i - 12}PM`;
-        }
-
-        return `${i}AM`
-    }
-
-    for (let i = 9; i < 18; i++) {
-        const timeblock = $('<div>')
-        .attr('id', `hour-${i}`)
-        .addclass('row time-block past');
-
-        timeblock.append($('<div>').addclass('col-md-1hour').text(formatTime(i)));
-        timeblock.append($('<textarea>').addclass('col-md-10 description'));
-        timeblock.append(
-            $('<button>')
-            .addclass('btn saveBtn col-md-1')
-            .append($('<i>').addclass('fas fa-save'))
-        );
-
-        container.append(timeBlock);
-    
-    }
-});
+function myfunction() {
+    console.log(currentDate);
+}
