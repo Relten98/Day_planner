18a8311b88628236d8b156dc2b38f85aaea2957f
Relten98 <73558735+Relten98@users.noreply.github.com>
2020-12-12 10:28:37 -0800

    improved formatting
    Now the site is very...
    
    a e s t h e t i c

---
+++ Assets/script.js
@@ -0,0 +1,14 @@
+var d = new Date();
+
+var month = d.getMonth()+1;
+var day = d.getDate();
+
+var output = d.getFullYear() + '/' +
+    (month<10 ? '0' : '') + month + '/' +
+    (day<10 ? '0' : '') + day;
+
+    console.log(d)
+
+
+
+$("container").append ( <div>d</div> )
